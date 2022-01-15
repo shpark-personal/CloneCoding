285cb2f320602b2d437d518898fde8682928e0c5
soheePark <5000roto@naver.com>
2022-01-15 11:25:02 +0900

    basic : 함수를 메서드화시킴

--- ReactBasic/basic/src/EventPractice.js
+++ ReactBasic/basic/src/EventPractice.js
@@ -5,6 +5,27 @@ class EventPractice extends Component{
     state = {
         message : 'gogo'
     }
+
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(e){
+        // 입력받은 내용으로 state를 변경
+        this.setState({
+            message : e.target.value
+        })
+    }
+
+    handleClick(){
+        // click 시 state를 출력 후 state값 초기화
+        alert(this.state.message);
+        this.setState({
+            message : ''
+        });
+    }
     
     render(){
         return (
@@ -14,23 +35,9 @@ class EventPractice extends Component{
                 type = "text"
                 name = "message"
                 placeholder = '아무거나 입력하세요'
-                onChange = {
-                    (e) =>{
-                        // 입력받은 내용으로 state를 변경
-                        this.setState({
-                            message : e.target.value
-                        })
-                    }
-                }/>
-                <button onClick={
-                    () => {
-                        // click 시 state를 출력 후 state값 초기화
-                        alert(this.state.message);
-                        this.setState({
-                            message : ''
-                        });
-                    }
-                }>확인</button>
+                value = {this.state.message}
+                onChange = {this.handleChange}/>
+                <button onClick={this.state.handleClick}>확인</button>
             </div>
         )
     }
