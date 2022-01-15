1eafdaf0ec6a9e45fbc4e1ec54fff3e95dc4a824
soheePark <5000roto@naver.com>
2022-01-15 11:36:14 +0900

    basic : input값이 여러개일 때 event객체 활용

--- ReactBasic/basic/src/EventPractice.js
+++ ReactBasic/basic/src/EventPractice.js
@@ -3,6 +3,7 @@ import {Component} from 'react'
 class EventPractice extends Component{
     
     state = {
+        username : '사용자명',
         message : 'gogo'
     }
 
@@ -15,14 +16,17 @@ class EventPractice extends Component{
     handleChange = (e) => {
         // 입력받은 내용으로 state를 변경
         this.setState({
-            message : e.target.value
+            // 객체 안에서 key를 []로 감싸면
+            // 그 안에 넣은 ref가 가리키는 실제 값이 key값으로 사용됨
+            [e.target.name] : e.target.value
         })
     }
 
     handleClick = () => {
         // click 시 state를 출력 후 state값 초기화
-        alert(this.state.message);
+        alert(this.state.username + ':' + this.state.message);
         this.setState({
+            username : '사용자명',
             message : ''
         });
     }
@@ -31,6 +35,12 @@ class EventPractice extends Component{
         return (
             <div>
                 <h1> 이벤트 연습</h1>
+                <input
+                type = "text"
+                name = "username"
+                placeholder = '사용자명'
+                value = {this.state.username}
+                onChange = {this.handleChange}/>
                 <input
                 type = "text"
                 name = "message"
