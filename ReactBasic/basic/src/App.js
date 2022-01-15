cdb83b7253e5a99275e76cabc60a534a5d2e2d55
soheePark <5000roto@naver.com>
2022-01-15 12:26:44 +0900

    basic : scrollbox 만들기 ( component에 ref 달기 )

--- ReactBasic/basic/src/App.js
+++ ReactBasic/basic/src/App.js
@@ -1,11 +1,19 @@
+import { Component } from 'react';
 import './App.css';
+import ScrollBox from './ScrollBox';
 // import EventPractice from './EventPractice';
 // import FirstComponent from './FirstComponent';
 // import Say from './Say';
-import ValidationSample from './ValidationSample';
+//import ValidationSample from './ValidationSample';
 
-function App() {
-  return  <ValidationSample/>
-};
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <ScrollBox />
+      </div>
+    );
+  }
+}
 
 export default App;
