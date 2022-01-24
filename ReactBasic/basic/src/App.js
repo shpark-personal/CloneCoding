3e7c9b832d8552b6f95b928d4a4b3be0e052933f
soheePark <5000roto@naver.com>
2022-01-24 22:41:29 +0900

    basic : component 렌더링 살펴보기

--- ReactBasic/basic/src/App.js
+++ ReactBasic/basic/src/App.js
@@ -1,17 +1,32 @@
 import { Component } from 'react';
 import './App.css';
-import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+//import IterationSample from './IterationSample';
 //import ScrollBox from './ScrollBox';
 // import EventPractice from './EventPractice';
 // import FirstComponent from './FirstComponent';
 // import Say from './Say';
 //import ValidationSample from './ValidationSample';
 
+function getRandomColor(){
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 class App extends Component {
+  state = {
+    color : '#000000'
+  }
+  handleClick = () =>{
+    this.setState({
+      color : getRandomColor()
+    });
+  }
+
   render(){
     return(
       <div>
-        <IterationSample />
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color = {this.state.color}/>
       </div>
     );
   }
