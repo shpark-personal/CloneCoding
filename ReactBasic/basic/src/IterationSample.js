330e47d9b5871b721ee3629d8faf07ccff036792
soheePark <5000roto@naver.com>
2022-01-24 21:29:08 +0900

    basic : 입력받은 data를 key와 함께 리스트로 만들기

--- ReactBasic/basic/src/IterationSample.js
+++ ReactBasic/basic/src/IterationSample.js
@@ -1,17 +1,39 @@
-const IterationSample = () =>{
-    // map함수 사용
-    var words = ['눈사람', '얼음', '눈', '바람'];
-    var nameList = words.map((name, index) => <li key={index}>{name}</li>);
-    return <ul>{nameList}</ul>
-    // return(
-    //     // 반복적으로 li생성
-    //     <ul>
-    //         <li>눈사람</li>
-    //         <li>얼음</li>
-    //         <li>눈</li>
-    //         <li>바람</li>
-    //     </ul>
-    // )
+import {useState} from 'react';
+
+const IterationSample = () => {
+    const [names, setNames] = useState([
+        {id : 1, text : '눈사람'},
+        {id : 2, text : '얼음'},
+        {id : 3, text : '눈'},
+        {id : 4, text : '바람'}
+    ]);
+
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(5);
+
+    const onChange = e => setInputText(e.target.value);
+    const onClick = () => {
+        const nextNames = names.concat({
+            id : nextId,
+            text : inputText
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText('');
+    }
+    
+    
+    const nameList = names.map(name => <li key = {name.id}>{name.text}</li>);
+    
+    
+    
+    return (
+    <>
+    <input value = {inputText} onChange={onChange}/>
+    <button onClick={onClick}> 추가 </button>
+    <ul>{nameList}</ul>
+    </>
+    );
 }
 
 export default IterationSample;
