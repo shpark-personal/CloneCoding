ae3b13b5fc55d3b24076ed045ee5d3b8749931f9
soheePark <5000roto@naver.com>
2022-01-24 21:19:39 +0900

    basic : map에서 index를 key 로 사용

--- ReactBasic/basic/src/IterationSample.js
+++ ReactBasic/basic/src/IterationSample.js
@@ -1,7 +1,7 @@
 const IterationSample = () =>{
     // map함수 사용
     var words = ['눈사람', '얼음', '눈', '바람'];
-    var nameList = words.map(name => <li>{name}</li>);
+    var nameList = words.map((name, index) => <li key={index}>{name}</li>);
     return <ul>{nameList}</ul>
     // return(
     //     // 반복적으로 li생성
