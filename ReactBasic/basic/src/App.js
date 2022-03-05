2b462dd7d7b407d44e521180c25d9f836f860fe4
soheePark <5000roto@naver.com>
2022-03-06 03:18:13 +0900

    hooks : useReducer

--- ReactBasic/basic/src/App.js
+++ ReactBasic/basic/src/App.js
@@ -1,16 +1,7 @@
 import Info from './Info';
-import {useState} from 'react';
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-  return (
-  <div>
-    <button onClick = {() => { setVisible(!visible)}}> 
-      {visible ? '숨기기' : '보이기'}
-    </button>
-    {visible && <Info />}
-  </div>
-  )
+  return <Info />
 };
 
 export default App;
--- ReactBasic/basic/src/Info.js
+++ ReactBasic/basic/src/Info.js
@@ -1,29 +1,25 @@
-import {useEffect, useState} from 'react';
+import {useReducer} from 'react';
+
+function reducer(state, action){
+    return{
+        ...state,
+        [action.name]:action.value
+    };
+}
 
 const Info = () =>{
-    const [name, setName] = useState('');
-    const [nickname, setNickname] = useState('');
-    useEffect(() => {
-        console.log('effect');
-        console.log(name);
-        return () => {
-            console.log('cleanup');
-            console.log(name);
-        };
-    }, [name]);
+    const [state, dispatch] = useReducer(reducer, {name:'', nickname:''});
+    const {name, nickname} = state;
 
-    const onChangeName = e =>{
-        setName(e.target.value);
-    }
-    const onChangeNickname = e =>{
-        setNickname(e.target.value);
+    const onChange = e => {
+        dispatch(e.target); // e.target이 reducer함수의 action으로 들어간다.
     }
 
     return(
         <div>
             <div>
-                <input value = {name} onChange={onChangeName}/>
-                <input value = {nickname} onChange={onChangeNickname}/>
+                <input name="name" value = {name} onChange={onChange}/>
+                <input name="nickname" value = {nickname} onChange={onChange}/>
             </div>
             <div>
                 <b>이름 : </b> {name}
