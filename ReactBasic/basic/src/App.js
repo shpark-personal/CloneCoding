7dc1fdb0ffe3affafd2e9afb119d9de4d6a54e86
soheePark <5000roto@naver.com>
2022-03-06 02:08:42 +0900

    hooks : useEffect

--- ReactBasic/basic/src/App.js
+++ ReactBasic/basic/src/App.js
@@ -1,7 +1,16 @@
 import Info from './Info';
+import {useState} from 'react';
 
 const App = () => {
-  return <Info />;
+  const [visible, setVisible] = useState(false);
+  return (
+  <div>
+    <button onClick = {() => { setVisible(!visible)}}> 
+      {visible ? '숨기기' : '보이기'}
+    </button>
+    {visible && <Info />}
+  </div>
+  )
 };
 
 export default App;
