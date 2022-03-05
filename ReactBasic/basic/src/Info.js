2479a29853e2a17ac7387a6a5099f0fe3b8ad905
soheePark <5000roto@naver.com>
2022-03-06 01:23:22 +0900

    hooks : 여러개의 useState

---
+++ ReactBasic/basic/src/Info.js
@@ -0,0 +1,28 @@
+import {useState} from 'react';
+
+const Info = () =>{
+    const [name, setName] = useState('');
+    const [nickname, setNickname] = useState('');
+
+    const onChangeName = e =>{
+        setName(e.target.value);
+    }
+    const onChangeNickname = e =>{
+        setNickname(e.target.value);
+    }
+
+    return(
+        <div>
+            <div>
+                <input value = {name} onChange={onChangeName}/>
+                <input value = {nickname} onChange={onChangeNickname}/>
+            </div>
+            <div>
+                <b>이름 : </b> {name}
+                <b>닉네임 : </b> {nickname}
+            </div>
+        </div>
+    )
+}
+
+export default Info;
