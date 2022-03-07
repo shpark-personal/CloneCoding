0a86cc0ce54541d123b981e9777225ebaed3a61c
Sohee Park <5000roto@naver.com>
2022-03-08 00:24:22 +0900

    component-styling : classname 적용하기

--- ReactBasic/styling-react/src/CssModule.js
+++ ReactBasic/styling-react/src/CssModule.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import styles from './CssModule.module.css';
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles); // 미리 style에서 class를 받아옴
 
 const CSSModule = () => {
   return (
-    <div className={`${styles.wrapper} ${styles.inverted}`}>
+    <div className={cx('wrapper', 'inverted')}>
       안녕하세요, 저는 <span className="something">CSS Module!</span>
     </div>
   );
