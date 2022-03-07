7a62e0b983b5292146c322efdcd8171cf12788b9
Sohee Park <5000roto@naver.com>
2022-03-08 00:03:01 +0900

    component-styling : css module 만들기

--- ReactBasic/styling-react/src/CssModule.js
+++ ReactBasic/styling-react/src/CssModule.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import classNames from 'classnames/bind';
-import styles from './CSSModule.module.scss';
+import styles from './CssModule.module.scss';
 
 const CSSModule = () => {
   return (
