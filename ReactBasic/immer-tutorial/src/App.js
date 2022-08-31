19b42030bf346749e2bca500ef558b4eff82f45f
Sohee Park <5000roto@naver.com>
2022-08-31 23:21:42 +0900

    immer사용 : immer 적용해보기

--- ReactBasic/immer-tutorial/src/App.js
+++ ReactBasic/immer-tutorial/src/App.js
@@ -1,4 +1,5 @@
 import {useRef, useCallback, useState} from 'react';
+import produce from 'immer';
 
 function App() {
   const nextId = useRef(1);
@@ -11,10 +12,11 @@ function App() {
   const onChange = useCallback(
     e => {
       const {name, value} = e.target;
-      setForm({
-        ...form,
-        [name] : [value]
-      });
+      setForm(
+        produce(form, draft => {
+          draft[name] = value;
+        })
+      );
     },
     [form]
   );
@@ -27,11 +29,12 @@ function App() {
       username : form.username
     };
 
-    setData({
-      ...data,
-      array: data.array.concat(info)
-    });
-
+    setData(
+      produce(data, draft => {
+        draft.array.push(info);
+      }
+    ));
+  
     setForm({
       name:'',
       username:''
@@ -42,11 +45,11 @@ function App() {
 
   const onRemove = useCallback(
     id => {
-      setData({
-        ...data,
-        array: data.array.filter(info => info.id !== id)
-      });
-    },
+      setData(
+        produce(data, draft => {
+          draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
+        }
+        ))},
     [data]
   );
   
