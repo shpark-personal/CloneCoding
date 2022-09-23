089115dd115a2975325932b59040377154ceead7
Sohee Park <5000roto@naver.com>
2022-09-23 23:48:51 +0900

    redux심화 : container 를 hook으로 전환

--- ReactBasic/react-redux-tutorial/src/containers/CounterContainer.js
+++ ReactBasic/react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,24 +1,19 @@
-import { connect } from 'react-redux';
+import { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter'; 
 
-const CounterContainer = ({number, increase, decrease}) => {
+const CounterContainer = () => {
+    const number = useSelector(state => state.counter.number);
+    const dispatch = useDispatch();
+    const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+    const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
     return (
-        <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+        <Counter number={number} 
+        onIncrease={onIncrease}
+        onDecrease={onDecrease}
+        />
     );
 };
 
-const mapStateToProps = state => ({
-    number: state.counter.number,
-});
-const mapDispatchToProps = dispatch => ({
-    increase:() => {
-        dispatch(increase());
-    },
-    decrease:() => {
-        dispatch(decrease());
-    },
-});
-// connect( store안의 상태를 컴포넌트의 props로 넘겨주기 위해 사용하는 함수,
-//          action생성 함수를 컴포넌트의 props로 넘겨주기 위해 사용하는 함수 )( 연동할 component )
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+export default CounterContainer;
