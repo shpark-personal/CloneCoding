9b373bb502ca1477d7447c6baf1f718e65c2fe86
Sohee Park <5000roto@naver.com>
2022-09-23 23:51:16 +0900

    redux심화 : useActions사용

--- ReactBasic/react-redux-tutorial/src/containers/TodosContainer.js
+++ ReactBasic/react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,5 +1,4 @@
-import {useCallback} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import {useSelector} from 'react-redux';
 import {changeInput, insert, toggle, remove} from '../modules/todos';
 import Todos from '../components/Todos';
 import useActions from '../lib/useActions'
@@ -9,11 +8,11 @@ const TodosContainer = () =>{
         input: todos.input,
         todos: todos.todos
     }));
-    const dispatch = useDispatch();
-    const onChangeInput = useCallback(input => dispatch(changeInput(input)), [dispatch]);
-    const onInsert = useCallback(text => dispatch(insert(text)), [dispatch])
-    const onToggle = useCallback(text => dispatch(toggle(text)), [dispatch])
-    const onRemove = useCallback(text => dispatch(remove(text)), [dispatch])
+
+    const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+        [changeInput, insert, toggle, remove],
+        []
+    );
     return (
         <Todos input = {input}
                 todos={todos}
