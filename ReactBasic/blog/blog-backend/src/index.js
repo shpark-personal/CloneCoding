12b7da11719c2b9a4a01a1d12fb73a74be0bf860
Sohee Park <5000roto@naver.com>
2022-11-21 21:41:20 +0900

    nodejs-koa : middleware 사용해보기

--- ReactBasic/blog/blog-backend/src/index.js
+++ ReactBasic/blog/blog-backend/src/index.js
@@ -1,1 +1,23 @@
-const hello = "hello";
+const Koa = require('koa');
+const app = new Koa();
+app.use((ctx, next) =>{
+    console.log(ctx.url);
+    console.log(1);
+    if(ctx.query.authorized !== '1'){
+        ctx.status = 401; // unauthorized
+        return;
+    }
+    next().then(() => {
+        console.log('End');
+    });
+});
+app.use((ctx, next) => {
+    console.log(2);
+    next();
+})
+app.use(ctx => {
+    ctx.body = 'hello world';
+})
+app.listen(4000, () => {
+    console.log('listening to port 4000');
+});
