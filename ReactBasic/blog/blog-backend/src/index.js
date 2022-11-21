b91dafcb6447101e1fe81f5522f9ce057d293492
Sohee Park <5000roto@naver.com>
2022-11-21 22:01:48 +0900

    nodejs-koa : api설정해보기

--- ReactBasic/blog/blog-backend/src/index.js
+++ ReactBasic/blog/blog-backend/src/index.js
@@ -1,22 +1,24 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+const api = require('./api');
+
 const app = new Koa();
-app.use(async (ctx, next) =>{
-    console.log(ctx.url);
-    console.log(1);
-    if(ctx.query.authorized !== '1'){
-        ctx.status = 401; // unauthorized
-        return;
-    }
-    await next();
-    console.log('End');
+const router = new Router();
+
+router.get('/', ctx => {
+    ctx.body = '홈';
 });
-app.use((ctx, next) => {
-    console.log(2);
-    next();
+router.get('/about/:name?', ctx => {
+    const { name } = ctx.params;
+    ctx.body = name ? `${name}의 소개` : '소개';
+});
+router.get('/posts', ctx => {
+    const { id } = ctx.query;
+    ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
 })
-app.use(ctx => {
-    ctx.body = 'hello world';
-})
+router.use('/api', api.routes());
+
+app.use(router.routes()).use(router.allowedMethods());
 app.listen(4000, () => {
     console.log('listening to port 4000');
 });
