a74c35eb5635b9ca5f32a268b51b7cd53dae2822
Sohee Park <5000roto@naver.com>
2022-11-30 22:31:45 +0900

    mongoose-setting

--- ReactBasic/blog/blog-backend/src/index.js
+++ ReactBasic/blog/blog-backend/src/index.js
@@ -1,6 +1,11 @@
+var process = require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+
+// process.env 내부 값에 대한 레퍼런스 만들기
+const {PORT} = process.env;
+
 const api = require('./api');
 
 const app = new Koa();
@@ -20,6 +25,8 @@ router.get('/posts', ctx => {
 router.use('/api', api.routes());
 
 app.use(router.routes()).use(router.allowedMethods());
-app.listen(4000, () => {
+
+const port = PORT || 4000;
+app.listen(port, () => {
     console.log('listening to port 4000');
 });
