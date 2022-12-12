0d9ba22aae847f423f1ce5fa05dab96e8bd649d8
Sohee Park <5000roto@naver.com>
2022-12-12 21:03:44 +0900

    mongoose : objectId 검증

--- ReactBasic/blog/blog-backend/src/api/posts/index.js
+++ ReactBasic/blog/blog-backend/src/api/posts/index.js
@@ -1,11 +1,15 @@
 //const Router = require('koa-router');
 import Router from 'koa-router';
 import * as postsCtrl from './posts.ctrl';
+
 const posts = new Router();
 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.patch('/:id', postsCtrl.update);
+
+const post = new Router();
+posts.get('/', postsCtrl.read);
+posts.delete('/', postsCtrl.remove);
+posts.patch('/', postsCtrl.update);
+posts.use('/:id', postsCtrl.checkObjectId, post.routes());
 export default posts;
